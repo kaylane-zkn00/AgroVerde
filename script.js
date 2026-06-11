ebe68994d3c25f258689efa33d7bd1e100bd777a
kaylane-zkn00 <kaylane.costa.souza@escola.pr.gov.br>
2026-06-11 09:45:18 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function respostaCorreta() {
+    document.getElementById("resultado").innerHTML =
+    "✅ Muito bem! Essa prática ajuda a proteger o meio ambiente.";
+}
+
+function respostaErrada() {
+    document.getElementById("resultado").innerHTML =
+    "❌ Essa prática prejudica o equilíbrio ambiental.";
+}
